ea062e2fa9a2b41c36f281d14672a4f315fa0ff0
rono516 <ronocollins2000@gmail.com>
2021-10-13 23:08:12 +0300

    second commit

---
+++ shop/resources/views/admin/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.admin')
+@section('content')
+ <div class="card">
+        <div class="card-body">
+            <h1>Rono Coder</h1>
+        </div>
+     </div>    
+@endsection
---
+++ shop/resources/views/layouts/inc/sidebar.blade.php
@@ -0,0 +1,33 @@
+<div class="sidebar" data-color="purple" data-background-color="white" data-image="../assets/img/sidebar-1.jpg">
+    <!--
+      Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+      Tip 2: you can also add an image using data-image tag
+  -->
+    <div class="logo"><a href="#" class="simple-text logo-normal">
+        Mega E-shop
+      </a></div>
+    <div class="sidebar-wrapper">
+      <ul class="nav">
+        <li class="nav-item active  ">
+          <a class="nav-link" href="./dashboard.html">
+            <i class="material-icons">dashboard</i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link" href="./user.html">
+            <i class="material-icons">person</i>
+            <p>User Profile</p>
+          </a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link" href="./tables.html">
+            <i class="material-icons">content_paste</i>
+            <p>Table List</p>
+          </a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
